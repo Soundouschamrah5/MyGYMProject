0df816baf824649ba99a7295949fe2cc8ffe3ee3
YounesBoubbou <102986644+YounesBoubbou@users.noreply.github.com>
2022-05-08 00:08:52 +0000

    Add files via upload

---
+++ MyGym_Data.sql
@@ -0,0 +1,11 @@
+insert into users (user_email, user_password, user_fname, user_lname, 
+				   user_age, gender, user_height, user_weight, user_trainingstatus)
+				   
+values  ('Y.Boubbou@aui.ma', 'Younes123', 'Younes', 'Boubbou', '20', 'M', '185', '106', 'I'), 
+		('H.Bouhamidi@aui.ma', 'Hachem101', 'Hachem', 'Bouhamidi', '20', 'M', '189', '85', 'A'),
+		('A.Ahenjir@aui.ma', 'AmineAh12', 'Amine', 'Ahenjir', '19', 'M', '170', '52', 'B'),
+		('S.charmah@aui.ma', 'Soundous05', 'Soundous', 'Chamrah', '20', 'F', '167', '65', 'A'),
+		('A.Mabkhout@aui.ma', 'Ayoub78', 'Ayoub', 'Mabkhout', '21', 'M', '180', '76', 'A'),
+		('A.Maimmadi@aui.ma', 'Ayoub505', 'Ayoub', 'Maimmadi', '21', 'M', '178', '65', 'A'),
+		('R.Alaoui@aui.ma', 'Rabii302', 'Rabii', 'Lamharzi', '20', 'M', '180', '85', 'B'),
+		('A.Benchekroun@aui.ma', 'Amine25', 'Amine', 'Becnhekroun', '23', 'M', '183', '70', 'B');
